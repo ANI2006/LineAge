1b2bd7f832b4fe4cdce081555204fd5209703c6a
Ani <sahakyanani703@gmail.com>
2023-05-26 13:02:30 +0400

    Relation Adding

---
+++ app/src/main/java/com/example/lineage6/mvvm/RelationViewModel.java
@@ -0,0 +1,62 @@
+package com.example.lineage6.mvvm;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.lineage6.db.AppDatabase;
+import com.example.lineage6.db.DatabaseClient;
+import com.example.lineage6.db.Relation;
+
+import java.util.List;
+
+public class RelationViewModel extends AndroidViewModel {
+    private AppDatabase appDatabase;
+    private MutableLiveData<List<Relation>> relations;
+
+    public RelationViewModel(@NonNull Application application) {
+        super(application);
+        relations = new MutableLiveData<>();
+        appDatabase = DatabaseClient.getInstance(getApplication()).getAppDatabase();
+        AsyncTask.execute(this::refreshRelationList);
+    }
+
+    public LiveData<List<Relation>> getRelations() {
+        return relations;
+    }
+
+    public void addRelation(@NonNull String person1,String person2,String relationBetween) {
+        AsyncTask.execute(() -> {
+            appDatabase.relationDao().insertRelation(new Relation(person1, person2,relationBetween));
+            refreshRelationList();
+        });
+    }
+
+    public void deleteRelation(Relation relation) {
+        AsyncTask.execute(() -> {
+            appDatabase.relationDao().deleteRelation(relation);
+            refreshRelationList();
+        });
+    }
+
+    public void refreshRelationList() {
+        List<Relation> updateList = appDatabase.relationDao().getAllRelations();
+        relations.postValue(updateList);
+    }
+    public void readRequest() {
+        AsyncTask.execute(() -> {
+            refreshRelationList();
+        });
+    }
+
+    public void updateRelation(Relation relation) {
+        AsyncTask.execute(() -> {
+            appDatabase.relationDao().updateRelation(relation);
+            refreshRelationList();
+        });
+    }
+}
